77c32f1e67eac8f842858aaac1491fb664b454d9
Klim Agarkov <jewelgatorstv@gmail.com>
2015-11-26 21:08:43 +0400

    add selection of the barber

--- app.rb
+++ app.rb
@@ -18,3 +18,34 @@ end
 get '/about' do
 	erb :about
 end
+
+
+post '/visit' do 
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+
+	@title = 'Thank You'
+	@message = "Дорогой #{@username} с нетерпением ждем  вас #{@datetime}"
+
+	f = File.open './public/users.txt', 'a'
+	f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}"
+	f.close
+
+	erb :message
+end
+
+	post '/contacts' do 
+	@email = params[:email]
+	@feedback = params[:feedback]
+	
+
+	@title = 'Thank You'
+	@message = "Спасибо за ваш отзыв. Он очень важен для нас (на самом деле нет)"
+
+	f = File.open './public/contacts.txt', 'a'
+	f.write "Email: #{@email}, Feedback: #{@feedback}"
+	f.close
+
+	erb :message
+end
