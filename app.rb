e66fd11ffbbca05e5f249d2af5b1f1e17e491cb1
Klim Agarkov <jewelgatorstv@gmail.com>
2016-01-16 22:55:12 +0400

    barber select

--- app.rb
+++ app.rb
@@ -5,18 +5,45 @@ require 'sinatra/reloader'
 require "mail"
 require 'sqlite3'
 
+def if_barber_exists? db, name
+	db.execute('SELECT * FROM Barbers WHERE name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+	barbers.each do |barber|
+		if !if_barber_exists? db, barber
+			db.execute 'INSERT INTO Barbers (name) values (?)', [barber]
+		end
+	end
+end
+
+def get_db
+	db = SQLite3::Database.new 'barbershop.sqlite'
+	db.results_as_hash = true
+	return db
+end
+
 configure do
-	@@db = SQLite3::Database.new 'barbershop.sqlite'
-	@@db.results_as_hash = true
-	@@db.execute 'CREATE TABLE IF NOT EXISTS
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
 		"Users" 
 		(
 			"id" INTEGER PRIMARY KEY  NOT NULL  DEFAULT (null),
 			"username" VARCHAR DEFAULT (null), 
 			"phone" TEXT,
-		 	"datestamp" TEXT, "barber" TEXT, 
+		 	"datestamp" TEXT, 
+		 	"barber" TEXT, 
 		 	"color" TEXT
 		)'
+	
+	db.execute 'CREATE TABLE IF NOT EXISTS
+	"Barbers"
+	(
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT
+	)'
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 
@@ -54,7 +81,7 @@ post '/visit' do
 		return erb :visit
 	end
 
-	@@db.execute 'INSERT INTO
+	db.execute 'INSERT INTO
 			Users (username, phone, datestamp, barber, color)
 			VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
@@ -117,8 +144,8 @@ end
 
 get '/showusers' do
 	
-	@@db.results_as_hash = true
-	@results = @@db.execute 'SELECT * FROM	Users ORDER BY id DESC'
+	
+	@results = db.execute 'SELECT * FROM	Users ORDER BY id DESC'
 	#	puts row               
 	#	username, phone, datestamp, barber, color)
 	#		VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
